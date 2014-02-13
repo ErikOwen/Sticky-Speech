7f14e093c971df1f5f65416029d0a223ea6ba289
Erik Owen <erikjowen@yahoo.com>
2014-02-13 11:26:21 -0800

    getting things stable again

--- src/com/spinninggangstaz/stickyspeech/Message.java
+++ src/com/spinninggangstaz/stickyspeech/Message.java
@@ -2,16 +2,23 @@ package com.spinninggangstaz.stickyspeech;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
+import android.text.format.Time;
+
 public class Message implements Serializable{
-	String text;
-	Date date;
+	private String text;
+	private Calendar date;
     private static final long serialVersionUID = 7526471155622776147L;
 	
-	public Message(String text) {//, Date date) {
+	public Message(String text) {
 		this.text = text;
-		//this.date = date;
+	}
+	
+	public Message(String text, Calendar date) {
+		this.text = text;
+		this.date = date;
 	}
 	
 	public String toString() {
@@ -20,5 +27,9 @@ public class Message implements Serializable{
 	
 	public void editText(String newText) {
 		this.text = newText;
+	}
+	
+	public void setDate(Calendar setDate) {
+		this.date = setDate;
 	}
 }
